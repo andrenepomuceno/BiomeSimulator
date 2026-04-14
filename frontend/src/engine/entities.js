c16a878bf55e5525fa39a02b404c695c28c39161
André Nepomuceno <andrerm2k@hotmail.com>
2026-04-13 22:40:48 -0300

    feat: Refactor reproduction logic by removing gestation handling from animal entities and behaviors

--- frontend/src/engine/entities.js
+++ frontend/src/engine/entities.js
@@ -70,8 +70,6 @@ export class Animal {
     this._nextThreatCheckTick = 0;
 
     this.mateCooldown = 0;
-    this.gestationTicksRemaining = 0;
-    this.gestationPartnerId = null;
     this.attackCooldown = 0;
     this._deathTick = null;
     this.consumed = false;
@@ -186,7 +184,6 @@ export class Animal {
 
     this.age++;
     if (this.mateCooldown > 0) this.mateCooldown--;
-    if (this.gestationTicksRemaining > 0) this.gestationTicksRemaining--;
     if (this.attackCooldown > 0) this.attackCooldown--;
     this._dirty = true;
   }
@@ -209,7 +206,6 @@ export class Animal {
       alive: this.alive,
       lifeStage: this.lifeStage,
       mateCooldown: this.mateCooldown,
-      gestationTicksRemaining: this.gestationTicksRemaining,
       attackCooldown: this.attackCooldown,
       targetX: this.targetX,
       targetY: this.targetY,
@@ -232,8 +228,6 @@ export class Animal {
       age: this.age,
       alive: this.alive,
       lifeStage: this.lifeStage,
-      mateCooldown: this.mateCooldown,
-      gestationTicksRemaining: this.gestationTicksRemaining,
       targetX: this.targetX,
       targetY: this.targetY,
       _deathTick: this._deathTick,
@@ -257,8 +251,6 @@ export class Animal {
       age: this.age,
       alive: this.alive,
       mateCooldown: this.mateCooldown,
-      gestationTicksRemaining: this.gestationTicksRemaining,
-      gestationPartnerId: this.gestationPartnerId,
       attackCooldown: this.attackCooldown,
       path: this.path,
       pathIndex: this.pathIndex,
